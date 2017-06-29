aa830a1c91783ce8074a1a2f0f2823e593adfc74
Bihju <bihju@Bihjus-MacBook-Pro-2.local>
2017-06-29 09:12:43 -0700

    Clean up code

--- ad-server/src/main/java/com/bihju/rest/AdServerController.java
+++ ad-server/src/main/java/com/bihju/rest/AdServerController.java
@@ -4,8 +4,10 @@ import com.bihju.AdEngine;
 import com.bihju.domain.Ad;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,11 +19,6 @@ import java.util.List;
 @Log4j
 public class AdServerController {
     private AdEngine adEngine;
-
-    @Value("${ui_template_file_path")
-    private String uiTemplate;
-    @Value("${ad_template_file_path")
-    private String adTemplate;
 
     @Autowired
     public AdServerController(AdEngine adEngine) {
@@ -40,25 +37,7 @@ public class AdServerController {
         String deviceIp = request.getParameter("dip");
         String queryCategory = request.getParameter("qclass");
 
-        List<Ad> ads = adEngine.selectAds(query, deviceId, deviceIp, queryCategory);
-//        String result = uiTemplate;
-        StringBuilder sb = new StringBuilder();
-        for (Ad ad : ads) {
-            log.info("Ad id = " + ad.getAdId());
-            log.info("Ad rank score = " + ad.getRankScore());
-            String adContent = adTemplate
-                    .replace("$title$", ad.title)
-                    .replace("brands$", ad.getBrand())
-                    .replace("$img$", ad.getThumbnail())
-                    .replace("$link$", ad.getDetailUrl())
-                    .replace("$price$", Double.toString(ad.getPrice()));
-            sb.append(adContent);
-        }
-
-//        result = result.replace("$list$", sb.toString());
-//        response.setContentType("text/html; charset=UTF-8");
-//        response.getWriter().write(result);
-        return ads;
+        return adEngine.selectAds(query, deviceId, deviceIp, queryCategory);
     }
 
     @RequestMapping(value = "ads", method = RequestMethod.GET)
